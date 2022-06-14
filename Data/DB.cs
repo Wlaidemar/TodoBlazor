1f7b70844e4bf08147d345dc1b30c5a15df150ce
Владимир Кузнецов <vkuznetsov@bf.local>
2022-06-14 16:13:16 +0600

    Todo

---
+++ Data/DB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class DB : DbContext
+    {
+        public DB(DbContextOptions<DB> options):base(options)
+        {}
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TodoList.Models;
+
+namespace TodoList.Data.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<TodoItem> GetAllItems();
+        void AddTodo(string todoName);
+        void ValueChanged(TodoItem changedItem);
+        void DeleteItem(int id);
+    }
+}
